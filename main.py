76287b7c3a2888e4b9f5e4905f7d78cefbb64f2f
chirbard <matamm@outlook.com>
2024-11-02 16:11:09 +0200

    not working

--- main.py
+++ main.py
@@ -2,14 +2,26 @@ import minimax
 import game_utils
 import numpy as np
 
+# board = [
+#     [1, 0, 1, 0, 1, 0],
+#     [0, 1, 0, 1, 0, 1],
+#     [0, 0, 0, 0, 0, 0],
+#     [0, 0, 0, 0, 0, 0],
+#     [-1, 0, -1, 0, -1, 0],
+#     [0, -1, 0, -1, 0, -1],
+# ]
+
 board = [
     [1, 0, 1, 0, 1, 0],
     [0, 1, 0, 1, 0, 1],
-    [0, 0, 0, 0, 0, 0],
+    [0, 0, -1, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
     [-1, 0, -1, 0, -1, 0],
-    [0, -1, 0, -1, 0, -1],
+    [0, 0, 0, -1, 0, -1],
 ]
 
 board = np.array(board)
 game_utils.checkers.print_board(board)
+
+# minimax.get_all_legal_moves(board, 1)
+minimax.bfs(board, 1, 1, 1)
